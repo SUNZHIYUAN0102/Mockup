8b834a0710e3c08a9b8b0d90efa1d656595be9a7
致远 孙 <494726814@qq.com>
2021-02-14 21:19:33 +0700

    Add project files.

---
+++ Mockup/Controllers/MockupController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mockup.Controllers
+{
+    public class MockupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListRole()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        public IActionResult EditRole()
+        {
+            return View();
+        }
+
+        public IActionResult AddOrRemove()
+        {
+            return View();
+        }
+
+    }
+}
